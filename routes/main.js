26962699b103560c642301a1b478abb879e28297
uknowsj <44722864+uknowsj@users.noreply.github.com>
2020-11-22 06:09:34 +0900

    m css

--- routes/main.js
+++ routes/main.js
@@ -6,12 +6,13 @@ router.get('/',function(req,res){
     res.render('index');
 });
 router.get('/targetInfoData',(req,res)=>{
-    Target.find(function(err, targets){
+    Target.find().sort({name:1}).exec(function(err, targets){
         if(err) {
           return res.status(500).send({error: err.message});
         }
         res.status(200).json(targets);
        });
 });
+
 
 module.exports = router;
